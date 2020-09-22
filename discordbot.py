60d26de6d868241873b44982fb319e51c5473b10
BellCocoa <36551213+BellCocoa@users.noreply.github.com>
2020-09-23 00:52:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,59 @@
+import discord
 from discord.ext import commands
+
 import os
-import traceback
+
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+if not discord.opus.is_loaded():
+    discord.opus.load_opus("heroku-buildpack-libopus")
+    
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@bot.command(aliases=["connect","summon"]) #connect or summonで呼び出しctx
+async def join(ctx):
+    """ボイスチャンネルに接続中"""
+    voice_state = ctx.author.voice
+    
+    if (not voice_state) or (not voice_state.channel):
+        await ctx.send("ボイスチャンネルに入ってから呼び出して下さい。")
+        return
+    
+   channel = voice_state.channel
 
+     await channel.connect()
+     print("connected to:",channel.name)
+        
+        
+ @bot.command(aliases=["disconnect","bye"])
+async def leave(ctx):
+    """チャンネルから切断しました。"""
+    voice_client = ctx.message.guild.voice_client
+    
+    if not voice_client:
+        await ctx.send("ボイスチャンネルに参加していません。")
+        return
+        
+        
+@bot.command(aliases=["test"])
+async def play(ctx):
+    """音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
+    
+    if not voice_client:
+        await ctx.send("ボイスチャンネルに参加していません。")
+        return
+    
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されていません。")
+        return
+    
+   await ctx.message.attachments[0].save("tmp.mp3")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+   ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+   voice_client.play(ffmpeg_audio_source)
 
-
+ await ctx.send("再生しました。")
+    
 bot.run(token)
