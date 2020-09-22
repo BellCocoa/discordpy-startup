ae45bee027455961fdbb69fb116edfc8629dbdee
BellCocoa <36551213+BellCocoa@users.noreply.github.com>
2020-09-23 01:12:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,57 +3,58 @@ from discord.ext import commands
 
 import os
 
-
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix="$")
 token = os.environ['DISCORD_BOT_TOKEN']
 
 if not discord.opus.is_loaded():
     discord.opus.load_opus("heroku-buildpack-libopus")
-    
 
-@bot.command(aliases=["connect","summon"]) #connect or summonで呼び出しctx
+@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
 async def join(ctx):
-    """ボイスチャンネルに接続中"""
+    """Botをボイスチャンネルに入室させます。"""
     voice_state = ctx.author.voice
-    
-    if (not voice_state) or (not voice_state.channel):
-        await ctx.send("ボイスチャンネルに入ってから呼び出して下さい。")
-        return
-    
-   channel = voice_state.channel
 
-     await channel.connect()
-     print("connected to:",channel.name)
-        
-        
- @bot.command(aliases=["disconnect","bye"])
+    if (not voice_state) or (not voice_state.channel):
+        await ctx.send("先にボイスチャンネルに入っている必要があります。")
+        return
+
+    channel = voice_state.channel
+
+    await channel.connect()
+    print("connected to:",channel.name)
+
+
+@bot.command(aliases=["disconnect","bye"])
 async def leave(ctx):
-    """チャンネルから切断しました。"""
+    """Botをボイスチャンネルから切断します。"""
     voice_client = ctx.message.guild.voice_client
-    
+
     if not voice_client:
-        await ctx.send("ボイスチャンネルに参加していません。")
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
         return
-        
-        
-@bot.command(aliases=["test"])
+
+    await voice_client.disconnect()
+    await ctx.send("ボイスチャンネルから切断しました。")
+
+
+@bot.command(aliases=[""])
 async def play(ctx):
-    """音声ファイルを流します。"""
+    """指定された音声ファイルを流します。"""
     voice_client = ctx.message.guild.voice_client
-    
+
     if not voice_client:
-        await ctx.send("ボイスチャンネルに参加していません。")
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
         return
-    
+
     if not ctx.message.attachments:
         await ctx.send("ファイルが添付されていません。")
         return
-    
-   await ctx.message.attachments[0].save("tmp.mp3")
 
-   ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
-   voice_client.play(ffmpeg_audio_source)
+    await ctx.message.attachments[0].save("tmp.mp3")
 
- await ctx.send("再生しました。")
-    
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生しました。")
+
 bot.run(token)
